3b31a363c26733dfcaf12a7a5fd5b689cd7dbd75
dojaNutz <116251577+dojaNutz@users.noreply.github.com>
2022-11-10 13:46:41 +0700

    Create TestJS.js

---
+++ Week4/TestJS.js
@@ -0,0 +1,15 @@
+//ตัวแปร
+var myName = "Kasidet";
+let myLName = "Supha";
+const year = 2002;
+/*year = 2023;*/
+
+//ปัดเศษ
+let grade = 3.56
+grade = Math.floor(grade);
+
+//Console.log
+console.log(grade);
+
+//Interpole 
+console.log(`My name is ${myName} ${myLName} i was born in ${year}`)
