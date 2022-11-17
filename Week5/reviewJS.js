b7df6555b0746ff6e2dc363b8ddd5d7085228540
dojaNutz <116251577+dojaNutz@users.noreply.github.com>
2022-11-17 15:52:59 +0700

    Create reviewJS.js

---
+++ Week5/reviewJS.js
@@ -0,0 +1,11 @@
+x = 150;
+var y = 100;
+let myName = 'Kasidet';
+const Y = 2022;
+//year = 2023;
+
+console.log('Year เป็นชนิด',typeof(Year));
+console.log('myName เป็นชนิด',typeof(myName));
+
+y='TNK';
+console.log(typeof(y));
