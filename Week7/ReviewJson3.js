0232dfd5807db34b600b48e39c45c3362585a813
dojaNutz <116251577+dojaNutz@users.noreply.github.com>
2022-12-01 11:37:43 +0700

    Create ReviewJson3.js

---
+++ Week7/ReviewJson3.js
@@ -0,0 +1,9 @@
+let obj = {
+    firstName: "Jack",
+    lastName: "Smith",
+    age: 25
+};
+
+//แปลง Object ให้เป็น String ด้วย JSON.stringify
+let myJSON = JSON.stringify(obj);
+console.log(myJSON);
